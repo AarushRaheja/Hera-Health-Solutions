582b0c206afc3202841c91e14aa8df35c9ccb132
Hari-Sagar <hari.sagar@outlook.com>
2025-07-02 18:53:09 -0700

    Fix member profile navigation and maintain member list visibility

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.user_dashboard, name='user_dashboard'),
+    path('user/<str:username>/', views.user_page, name='user_page'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,13 @@
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
 from .models import File
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def user_dashboard(request):
     user_files = File.objects.filter(user=request.user)
+    all_users = User.objects.all()
 
     if request.method == 'POST':
         for file_id in request.POST.getlist('file_id'):
@@ -16,4 +18,27 @@ def user_dashboard(request):
                 file.save()
         return redirect('user_dashboard')
 
-    return render(request, 'dashboard/user_dashboard.html', {'files': user_files})
+    return render(request, 'dashboard/user_dashboard.html', {
+        'files': user_files,
+        'all_users': all_users
+    })
+
+@login_required
+def user_page(request, username):
+    user = get_object_or_404(User, username=username)
+    user_files = File.objects.filter(user=user)
+    
+    if request.method == 'POST':
+        for file_id in request.POST.getlist('file_id'):
+            file = File.objects.get(id=file_id)
+            new_status = request.POST.get(f'status_{file_id}')
+            if new_status in dict(File.STATUS_CHOICES):
+                file.status = new_status
+                file.save()
+        return redirect('user_page', username=username)
+
+    return render(request, 'dashboard/user_dashboard.html', {
+        'files': user_files,
+        'user': user,
+        'all_users': User.objects.all()
+    })
